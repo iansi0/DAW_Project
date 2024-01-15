8a2733590c7630242fa785472b8106112960d725
bust20 <Braianpb02@gmail.com>
2024-01-15 20:37:58 +0100

    DOING: Crear layout y css + css tickets

--- ControlReparacions/app/Views/ticket.php
+++ ControlReparacions/app/Views/ticket.php
@@ -1,92 +1,77 @@
-<!DOCTYPE html>
-<html lang="ca">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tickets</title>
-</head>
-<body>
-  <nav id="Sidebar">
-    <a href="#">Alumnos</a>
-    <a href="#">Tickets</a>
-    <a href="#">Archivos</a>
-  </nav>
-  <section id="tickets">
-    <header>
-      <img src="logo.png" alt="Logo">
-      <h1>Nom Exemple</h1>
-    </header>
-    <h1>TICKETS</h1>
-    <h2>22-10-2023</h2>
-    <button style="margin-left: 50%;"  id="add-ticket">Afegir</button>
-    <table>
-      <thead>
-        <th>Ticket</th>
-        <th>ID_Disp</th>
-        <th>Type</th>
-        <th>Institut</th>
-        <th>Inici</th>
-        <th>Última</th>
-        <th>Estat</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>1234-5678-9012</td>
-        <td>1234567890</td>
-        <td>Portatil</td>
-        <td>Institut Escola del Treball</td>
-        <td>22-10-2023</td>
-        <td>22-10-2023</td>
-        <td>En reparació</td>
-      </tr>
-      <tr>
-        <td>1234-5678-9032</td>
-        <td>1234577890</td>
-        <td>Projector</td>
-        <td>Institut Escola del Treball</td>
-        <td>22-10-2023</td>
-        <td>22-10-2023</td>
-        <td>Reparat</td>
-      </tr>
-      <tr>
-        <td>2134-5678-9012</td>
-        <td>987654311</td>
-        <td>Torre</td>
-        <td>Institut Escola del Treball</td>
-        <td>22-10-2023</td>
-        <td>22-10-2023</td>
-        <td>Preparat per recollir</td>
-      </tr>
-      <tr>
-        <td>1234-5678-9012</td>
-        <td>098765432</td>
-        <td>Pantalla</td>
-        <td>Institut Escola del Treball</td>
-        <td>22-10-2023</td>
-        <td>22-10-2023</td>
-        <td>Validat i reparat</td>
-      </tr>
-      <tr>
-        <td>1234-5678-9012</td>
-        <td>098765434</td>
-        <td>Portatil</td>
-        <td>Institut Escola del Treball</td>
-        <td>22-10-2023</td>
-        <td>22-10-2023</td>
-        <td>Espatllat</td>
-      </tr>
-      <tr>
-        <td>1234-5678-9999</td>
-        <td>1234567899</td>
-        <td>Portatil</td>
-        <td>Institut Escola del Treball</td>
-        <td>22-10-2023</td>
-        <td>22-10-2023</td>
-        <td>Desguaçat</td>
-      </tr>
-      </tbody>
-    </table>
-  </section>
-</body>
-</html>
+<?= $this->extend('layouts/master.php') ?>
+
+
+<?= $this->section('content') ?>
+<h1 class="text-center text-7xl text-primario">TICKETS</h1>
+
+<button id="add-ticket" class=" bg-primario text-white float-right mb-5 px-5 py-2 hover:bg-terciario-4">+ Afegir</button>
+
+<table class="w-full ">
+  <thead class=" bg-primario text-white">
+    <th>Ticket</th>
+    <th>ID_Disp</th>
+    <th>Type</th>
+    <th>Institut</th>
+    <th>Inici</th>
+    <th>Última</th>
+    <th>Estat</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1234-5678-9012</td>
+      <td>1234567890</td>
+      <td>Portatil</td>
+      <td>Institut Escola del Treball</td>
+      <td>22-10-2023</td>
+      <td>22-10-2023</td>
+      <td>En reparació</td>
+    </tr>
+    <tr>
+      <td>1234-5678-9032</td>
+      <td>1234577890</td>
+      <td>Projector</td>
+      <td>Institut Escola del Treball</td>
+      <td>22-10-2023</td>
+      <td>22-10-2023</td>
+      <td>Reparat</td>
+    </tr>
+    <tr>
+      <td>2134-5678-9012</td>
+      <td>987654311</td>
+      <td>Torre</td>
+      <td>Institut Escola del Treball</td>
+      <td>22-10-2023</td>
+      <td>22-10-2023</td>
+      <td>Preparat per recollir</td>
+    </tr>
+    <tr>
+      <td>1234-5678-9012</td>
+      <td>098765432</td>
+      <td>Pantalla</td>
+      <td>Institut Escola del Treball</td>
+      <td>22-10-2023</td>
+      <td>22-10-2023</td>
+      <td>Validat i reparat</td>
+    </tr>
+    <tr>
+      <td>1234-5678-9012</td>
+      <td>098765434</td>
+      <td>Portatil</td>
+      <td>Institut Escola del Treball</td>
+      <td>22-10-2023</td>
+      <td>22-10-2023</td>
+      <td>Espatllat</td>
+    </tr>
+    <tr>
+      <td>1234-5678-9999</td>
+      <td>1234567899</td>
+      <td>Portatil</td>
+      <td>Institut Escola del Treball</td>
+      <td>22-10-2023</td>
+      <td>22-10-2023</td>
+      <td>Desguaçat</td>
+    </tr>
+  </tbody>
+</table>
+<?= $this->endSection() ?>
