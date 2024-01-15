35d5c5974b11098d8c15c3d2a1f6abb8381774a6
Hbances <hbances@inscaparrella.cat>
2024-01-15 18:44:53 +0100

    feat : create Views ticked

---
+++ ControlReparacions/app/Views/ticket.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tickets</title>
+</head>
+<body>
+  <nav id="Sidebar">
+    <a href="#">Alumnos</a>
+    <a href="#">Tickets</a>
+    <a href="#">Archivos</a>
+  </nav>
+  <section id="tickets">
+    <header>
+      <img src="logo.png" alt="Logo">
+      <h1>Nom Exemple</h1>
+    </header>
+    <h1>TICKETS</h1>
+    <h2>22-10-2023</h2>
+    <button style="margin-left: 50%;"  id="add-ticket">Afegir</button>
+    <table>
+      <thead>
+        <th>Ticket</th>
+        <th>ID_Disp</th>
+        <th>Type</th>
+        <th>Institut</th>
+        <th>Inici</th>
+        <th>Última</th>
+        <th>Estat</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1234-5678-9012</td>
+        <td>1234567890</td>
+        <td>Portatil</td>
+        <td>Institut Escola del Treball</td>
+        <td>22-10-2023</td>
+        <td>22-10-2023</td>
+        <td>En reparació</td>
+      </tr>
+      <tr>
+        <td>1234-5678-9032</td>
+        <td>1234577890</td>
+        <td>Projector</td>
+        <td>Institut Escola del Treball</td>
+        <td>22-10-2023</td>
+        <td>22-10-2023</td>
+        <td>Reparat</td>
+      </tr>
+      <tr>
+        <td>2134-5678-9012</td>
+        <td>987654311</td>
+        <td>Torre</td>
+        <td>Institut Escola del Treball</td>
+        <td>22-10-2023</td>
+        <td>22-10-2023</td>
+        <td>Preparat per recollir</td>
+      </tr>
+      <tr>
+        <td>1234-5678-9012</td>
+        <td>098765432</td>
+        <td>Pantalla</td>
+        <td>Institut Escola del Treball</td>
+        <td>22-10-2023</td>
+        <td>22-10-2023</td>
+        <td>Validat i reparat</td>
+      </tr>
+      <tr>
+        <td>1234-5678-9012</td>
+        <td>098765434</td>
+        <td>Portatil</td>
+        <td>Institut Escola del Treball</td>
+        <td>22-10-2023</td>
+        <td>22-10-2023</td>
+        <td>Espatllat</td>
+      </tr>
+      <tr>
+        <td>1234-5678-9999</td>
+        <td>1234567899</td>
+        <td>Portatil</td>
+        <td>Institut Escola del Treball</td>
+        <td>22-10-2023</td>
+        <td>22-10-2023</td>
+        <td>Desguaçat</td>
+      </tr>
+      </tbody>
+    </table>
+  </section>
+</body>
+</html>
