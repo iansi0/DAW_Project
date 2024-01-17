dd206d76f57452ca07c9067d4d679978ecd972ce
bust20 <Braianpb02@gmail.com>
2024-01-17 17:38:22 +0100

    FIX: Posicionamiento barra busqueda

--- ControlReparacions/app/Views/assignar.php
+++ ControlReparacions/app/Views/assignar.php
@@ -4,16 +4,15 @@
 <?= $this->section('content') ?>
 <h1 class="text-center text-7xl text-primario">ASSIGNAR</h1>
 
-<section class="flex justify-between mt-8">
-    <button class="bg-primario text-white mb-5 px-5 py-2 hover:bg-terciario-4">+Filter</button>
+<section class="flex  gap-8 mt-8  mb-5">
+    <button class="bg-primario text-white px-5 py-2 hover:bg-terciario-4">+Filter</button>
 
-    <form action="" class="flex items-center">
-        <label for="gsearch" class="hidden">buscador</label>
-        <input type="search" id="gsearch" name="gsearch" class="text-black bg-slate-400 ">
-        <input type="submit" class="bg-primario text-white hover:bg-terciario-4 ">
+    <form action="" class="flex gap-2 w-full">
+        <input type="search" id="gsearch" name="gsearch" class="text-black bg-slate-400 ml-auto pl-2 ">
+        <input type="submit" class="bg-primario text-white px-5 py-2 hover:bg-terciario-4">
     </form>
 
-    <button id="add-ticket" class="bg-primario text-white mb-5 px-5 py-2 hover:bg-terciario-4">Afegir</button>
+    <button id="add-ticket" class="bg-primario text-white px-5 py-2 hover:bg-terciario-4">Afegir</button>
 </section>
 
 
