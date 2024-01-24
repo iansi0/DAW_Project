461bb0afc3c66333adb7da73fbf25b546424543b
Hbances <hbances@inscaparrella.cat>
2024-01-24 17:23:42 +0100

    FEAT : tabla de alumnos

--- ControlReparacions/app/Config/Routes.php
+++ ControlReparacions/app/Config/Routes.php
@@ -8,3 +8,7 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/{locale}', 'Home::index');
 $routes->get('{locale}/tickets', 'Home::tickets');
 $routes->get('{locale}/assignar', 'Home::assignar');
+$routes->get('alumnos', 'Home::alumnos');
+$routes->get('intervencion','Home::intervencion');
+$routes->get('ticketinfo','Home::ticketinfo');
+$routes->get('formestudiantes','Home::formestudiantes');
