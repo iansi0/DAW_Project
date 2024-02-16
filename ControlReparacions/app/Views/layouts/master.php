9ff43f1e4bf35c475b7c9f6563d5ac7890240452
Ian Florensa <ianfloren@gmail.com>
2024-02-16 15:13:08 +0100

    Revert "Merge remote-tracking branch 'origin/login-function'"
    This reverts commit a0caa0589760a5101985a632dcd3d5c6e88a1498, reversing
    changes made to f31992caadc3bad83c93156a5ede2ff5c259afe7.

--- ControlReparacions/app/Views/layouts/master.php
+++ ControlReparacions/app/Views/layouts/master.php
@@ -1,8 +1,3 @@
-<?php
-$session = \Config\Services::session();
-$name = $_SESSION['username'];
-
-?>
 
 <!DOCTYPE html>
 <html lang="ca" class="h-full">
@@ -14,47 +9,19 @@ $name = $_SESSION['username'];
     <title>KYS</title>
 </head>
 
-<style>
-    body {
-        font-family: monospace;
-        /* font-family: system-ui; */
-    }
-</style>
-
 <body class=" flex h-full">
 
-    <nav id="Sidebar" class="bg-terciario-2 flex flex-col gap-10 py-20 w-20  text-5xl h-full ">
+    <nav id="Sidebar" class="bg-terciario-3 flex flex-col gap-10 py-20 w-20  text-5xl h-full ">
+        <a href="#" class=" w-20 overflow-hidden  transition-all ease-in duration-500   hover:w-60  hover:bg-primario hover:text-white hover:z-10"><?=strtoupper(lang('master.students'))?></a>
 
 
-        <a href="<?= base_url('alumnes') ?>" class=" w-20  h-16 overflow-hidden flex items-center text-2xl text-primario  transition-all ease-in duration-300  hover:w-56  hover:bg-primario hover:text-white">
-            <!-- Imagen ticket-->
-            <p class=" "><svg xmlns="http://www.w3.org/2000/svg" width="80" height="100" fill="currentColor" viewBox="-160 0 798 512">
-                    <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                </svg>
-            </p>
-            <b>ALUMNES</b>
-        </a>
+        <a href="#" class=" w-20   h-20 overflow-hidden flex items-center  transition-all ease-in duration-500   hover:w-60  hover:bg-primario hover:text-white hover:z-10">
+            <!-- Imagen ticket
+            <img src="/assets/img/navbar/ticketRojo.png" alt="ticket" class="w-20 h-20">-->
+            <?=strtoupper(lang('master.tickets'))?></a>
 
-        <!--TODO cuando haga click cambiar estilos con JS para marcar la view activa -->
-        <button class="hover:z-10">
-            <a href="<?= base_url('tickets') ?>" class=" w-20 h-16 overflow-hidden flex items-center text-2xl text-primario transition-all ease-in duration-300   hover:w-56  hover:bg-primario hover:text-white ">
-                <!-- Imagen ticket-->
-                <p class=" "><svg xmlns="http://www.w3.org/2000/svg" width="80" height="100" fill="currentColor"  viewBox="-3 0 24 16">
-                        <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6V4.5ZM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5h-13Z" />
-                    </svg>
-                </p>
-                <b>TICKETS</b>
-            </a>
-        </button>
 
-        <a href="<?= base_url('assignar') ?>" class=" w-20 h-16 overflow-hidden flex items-center text-2xl  transition-all ease-in duration-300   hover:w-56  hover:bg-primario hover:text-white hover:z-10">
-            <!-- Imagen ticket-->
-            <p class=" "><svg xmlns="http://www.w3.org/2000/svg" width="80" height="100" fill="currentColor" class="bi bi-ticket" viewBox="-3 0 24 16">
-                    <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6V4.5ZM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5h-13Z" />
-                </svg>
-            </p>
-            <b>ASSIGNAR</b>
-        </a>
+        <a href="#" class=" w-20 overflow-hidden  transition-all ease-in duration-500   hover:w-60  hover:bg-primario hover:text-white hover:z-10"><?=strtoupper(lang('master.assign'))?></a>
     </nav>
 
 
@@ -62,8 +29,7 @@ $name = $_SESSION['username'];
 
         <header class=" bg-primario text-white h-16 flex items-center relative justify-center   ">
             <img src="/assets/img/logo.png" alt="Logo">
-            <h1 class=" absolute right-0 mr-5"><?= $name ?></h1>
-
+            <h1 class=" absolute right-0 mr-5">Nom Exemple</h1>
         </header>
 
         <article class="p-5">
--- ControlReparacions/app/Views/login.php
+++ ControlReparacions/app/Views/login.php
@@ -8,20 +8,6 @@
     <link rel="stylesheet" href="<?= base_url() ?>/assets/css/styles.css">
 </head>
 
-<style>
-    body {
-        font-family: monospace;
-        /* font-family: system-ui; */
-    }
-</style>
-
-<body class="h-screen flex flex-col justify-center items-center bg-segundario">
-
-    <section class="bg-terciario-1 text-segundario flex flex-col gap-5 items-center border-2 rounded-md px-20 py-10">
-        <img src="/assets/img/logo.png" alt="Logo">
-        <form class=" flex flex-col   w-60 " method="POST" action="<?= base_url() ?>/login">
-            <label class="mb-1 text-xl" for="email">Email</label>
-            <input class="mb-5 rounded-md border-2 text-terciario-1 pl-1 focus:outline-none" type="email" id="email" name="email" />
 <body class=" h-screen flex flex-col justify-center items-center">
     <section class=" bg-gray-300 flex flex-col gap-5 items-center border-2 rounded-md border-black px-20 py-10">
         <h1 class=" text-7xl">KYS</h1>
@@ -29,17 +15,12 @@
             <label class="mb-1 text-xl" for="email"><?=lang('login.email')?></label>
             <input class="mb-5 rounded-md" type="email" id="email" />
 
-            <label class="mb-1 text-xl" for="password">Contrasenya</label>
-            <input class="mb-10 rounded-md border-2 text-terciario-1 pl-1 focus:outline-none" type="password" id="password" name="password" />
+            <label class="mb-1 text-xl" for="password"><?=lang('login.password')?></label>
+            <input class="mb-5 rounded-md  w-60 h-6" type="password" id="password" />
 
-            <input class="mb-5 text-lg py-2 rounded-sm bg-primario   hover:bg-terciario-2 hover:text-terciario-1" type="submit" value="INICIAR SESSIÓ">
+            <input class="mb-5 text-xl bg-gray-200 rounded-md" type="submit" value="<?=lang('login.login')?>">
 
-            <?php if (isset($error) && !empty($error)) : ?>
-
-                <p class="  text-primario text-center  text-lg r py-2"> <strong><?= $error ?></strong></p>
-            <?php endif ?>
-
-            <a class=" text-center" href="#">Has oblidat la contrasenya?</a>
+            <a href="#"><?=lang('login.forgetPassword')?></a>
         </form>
     </section>
 </body>
