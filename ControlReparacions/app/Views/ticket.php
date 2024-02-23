dbc3d1a568124a51404d20fa6246fba2daf16ffe
braian <Braianpb02@gmail.com>
2024-02-23 15:37:22 +0100

    feat: cambio de estilos general

--- ControlReparacions/app/Views/ticket.php
+++ ControlReparacions/app/Views/ticket.php
@@ -3,11 +3,11 @@
 <?= $this->section('content') ?>
 <h1 class="text-center text-7xl text-primario">TICKETS</h1>
 
-<button id="add-ticket" class=" bg-primario text-white float-right mb-5 px-5 py-2 hover:bg-terciario-4">+ Afegir</button>
+<button id="add-ticket" class=" bg-primario text-segundario float-right mb-5 px-5 py-2 hover:bg-terciario-4">+ Afegir</button>
 
 
 <table class="w-full ">
-  <thead class=" bg-primario text-white">
+  <thead class=" bg-primario text-segundario">
     <th>Ticket</th>
     <th>ID_Disp</th>
     <th>Type</th>
