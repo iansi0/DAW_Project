3c1c5fe1ba3b64ee2cdbc189d7bf7b2d2058ac5f
braian <Braianpb02@gmail.com>
2024-04-27 04:08:26 +0200

    show password button

--- ControlReparacions/app/Views/login.php
+++ ControlReparacions/app/Views/login.php
@@ -51,7 +51,7 @@
             <div class="mt-8">
                 <div class="relative flex items-center">
                     <input name="password" id="password" type="password" required class="w-full  text-sm border-b border-gray-300 focus:border-[#333] px-2 py-3 outline-none" placeholder="<?= lang('forms.passwd') ?>" />
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2 cursor-pointer" viewBox="0 0 128 128">
+                    <svg xmlns="http://www.w3.org/2000/svg" id="togglePasswordBtn" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2 cursor-pointer" viewBox="0 0 128 128">
                         <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
                     </svg>
                 </div>
@@ -75,7 +75,7 @@
 
             <!-- error -->
             <?php if (session()->getFlashdata('error')) : ?>
-                <div id="alert-border-2" class="flex items-center p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50  role="alert">
+                <div id="alert-border-2" class="flex items-center p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50  role=" alert">
                     <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
                     </svg>
@@ -101,5 +101,16 @@
 
 
 </body>
+
+<script>
+    const password = document.getElementById('password');
+    const buttonShow = document.getElementById('togglePasswordBtn');
+
+    togglePasswordBtn.addEventListener('click', () => {
+        const isPasswordVisible = password.type === 'text';
+        password.type = isPasswordVisible ? 'password' : 'text';
+       
+    });
+</script>
 
 </html>
