6e9f19f4a4f8e2921dc9c7b340668ccc57c1d0a2
Ian Florensa <ianfloren@gmail.com>
2024-05-16 04:00:21 +0200

    Feat: PDF export w/o styles

--- ControlReparacions/app/Controllers/PDFController.php
+++ ControlReparacions/app/Controllers/PDFController.php
@@ -3,18 +3,71 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\EstatModel;
+use App\Models\IntervencioModel;
+use App\Models\TiquetModel;
 use CodeIgniter\HTTP\ResponseInterface;
 use Dompdf\Dompdf;
 
 class PDFController extends BaseController
 {
-    public function index($route)
+    public function index($id)
     {
-        $dompdf = new Dompdf();
-        $html = view('pdfTicket'); 
+        if ($id == null) {
+            return redirect()->to(base_url('/tickets'));
+        }
+
+        $modelTickets = new TiquetModel();
+        $modelInterventions = new IntervencioModel();
+        $estat = new EstatModel();
+
+        /** TABLE GENERATOR **/
+        $table = new \CodeIgniter\View\Table();
+        $table->setHeading(
+            mb_strtoupper(lang('forms.date'), 'utf-8'),
+            mb_strtoupper(lang('titles.students'), 'utf-8'),
+            mb_strtoupper(lang('titles.material_2'), 'utf-8'),
+            mb_strtoupper(lang('forms.description'), 'utf-8')
+        );
+
+        $template = [
+            'table_open'  => "<table class='w-full'>",
+
+            'thead_open'  => "<thead class='bg-primario text-secundario'>",
+
+            'heading_cell_start' => "<th class='py-3 text-base'>",
+
+            'row_start' => "<tr class='border-b-[0.01px] '>",
+            'row_alt_start' => "<tr class='border-b-[0.01px]  bg-terciario-2'>",
+
+
+        ];
+        $table->setTemplate($template);
+
+        $data = [
+            'ticket' => $modelTickets->viewTicket($id),
+            'interventions' => $modelInterventions->getInterventions($id),
+            'pager' => $modelInterventions->pager,
+            'table' => $table,
+            'estats' => $estat->getAllStates(),
+        ];
+
+        foreach ($data['interventions'] as $intervencio) {
+            $buttonView = base_url("tickets/" . $intervencio['id']); // Reemplazar con tu ruta real
+
+            $table->addRow(
+                $intervencio['created_at'],
+                $intervencio['correu_alumne'],
+                $intervencio['id_tipus'],
+
+                ['data' => $intervencio['descripcio'], 'class' => $intervencio['id_tipus'] == 2 ? 'bg-red-500 text-segundario' : 'bg-segundario']
+            );
+        }        $dompdf = new Dompdf();
+        
+        $html = view('pdfTicket',$data); 
         $dompdf->loadHtml($html);
         $dompdf->render();
-        $dompdf->stream("ticket_".$route.".pdf", array("Attachment" => false));
-        $dompdf->down
+        $dompdf->stream("ticket_".$id.".pdf", array("Attachment" => true));
+        
     }
 }
--- ControlReparacions/app/Views/pdfTicket.php
+++ ControlReparacions/app/Views/pdfTicket.php
@@ -1,44 +1,87 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Exportar PDF</title>
+    <title>Ticket: <?=$ticket['id']?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="view-transition" content="same-origin">
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css">
+
     <style>
         body {
-            font-family: 'Arial', sans-serif;
-        }
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
+            /* font-family: monospace; */
+            font-family: system-ui;
+            /*montserrat para titulos con bold
+            lato y popins para textos medium */
         }
     </style>
+    <!-- <?php 
+        if (session('user')['user']=="admin") {
+            $theme ='dark';
+        }else{
+            $theme ='light';
+            
+        }
+    ?>
+    <script>
+        ='<?= $theme?>';
+        console.log( );
+    </script> -->
 </head>
 <body>
-    <h1>Listado de Usuarios</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Email</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- Aquí puedes cargar datos dinámicamente con PHP -->
-            <tr>
-                <td>1</td>
-                <td>Juan Pérez</td>
-                <td>juan@example.com</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Ana López</td>
-                <td>ana@example.com</td>
-            </tr>
-        </tbody>
-    </table>
+    
+<div class="flex gap-16 items-center  text-primario p-3 rounded-lg pl-5 w-full mb-3">
+
+<a href="<?= strpos(previous_url(), 'tickets?') !== false
+                ? str_replace('index.php/', '', previous_url())
+                : base_url('/tickets'); ?>">
+    <button id="pdf" class="hover:bg-[#FFB053] hover:text-terciario-1 p-2 px-3 rounded-xl transition hover:ease-in ease-out duration-250"><i class="fa-solid fa-arrow-left text-3xl"></i></button>
+</a>
+<h1 class=" text-left text-5xl ml-48"><?= mb_strtoupper(lang('titles.id_ticket'), 'utf-8'); ?>: <?= explode("-", $ticket['id'])[4] ?></h1>
+</div>
+
+<main style="view-transition-name: info<?= $ticket['id'] ?>;" class="flex gap-7 py-1 ">
+
+<section class="flex flex-col gap-2">
+
+
+    
+
+    <div class=" text-secundario min-w-72 max-w-80 rounded-t-lg overflow-hidden">
+        <h3 class="bg-primario text-lg p-3"> <?= lang('forms.info'); ?> </h3>
+        <p class="bg-terciario-2 p-2 text-terciario-1 overflow-auto"><i class="fa-solid fa-hashtag"></i> : <span class="text-sm"><?= $ticket['id'] ?></span></p>
+        <p class="bg-terciario-2 p-2 text-terciario-1 overflow-auto"><i class="fa-solid fa-envelope"></i> : <span class="text-sm"><?= $ticket['correu_contacte'] ?></span></p>
+    </div>
+
+
+    <div class=" text-secundario min-w-64 max-w-72  rounded-t-lg overflow-hidden">
+        <h3 class="bg-primario text-lg p-3"><?= lang('forms.description'); ?></h3>
+        <p class="bg-terciario-2 p-3 text-terciario-1  min-h-auto max-h-32 overflow-y-auto break-words"><?= $ticket['descripcio'] ?></p>
+    </div>
+
+</section>
+
+<article class="flex flex-col gap-2 w-full">
+
+    <div class="flex justify-between gap-4">
+   
+
+    </div>
+
+    <div>
+        <div class="flex justify-between bg-primario text-secundario text-left p-3 pr-8 text-3xl rounded-t-2xl">
+            <h1><?= lang('titles.int'); ?></h1>
+            
+
+        </div>
+
+        <?php
+        echo $table->generate();
+        ?>
+    </div>
+
+</article>
+</main>
 </body>
 </html>
