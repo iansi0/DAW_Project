b936cf36b6cfcf4a5068f07b9b1b9ac8027536b6
braian <Braianpb02@gmail.com>
2024-06-10 17:00:25 +0200

    fix: login xtec

--- ControlReparacions/app/Language/ca/login.php
+++ ControlReparacions/app/Language/ca/login.php
@@ -2,7 +2,7 @@
 return [
     'forget'    => 'Has oblidat la contrasenya?',
     'remember'  => 'Recorda\'m',
-    'google'    => 'Google',
+    'google'    => 'XTEC',
     'next'      => 'O inicia amb',
     'text'      => 'No hi ha registre de cap centre assignat en aquest compte xtec, si us plau introdueix el codi de centre que estàs actualment.',
 ];
--- ControlReparacions/app/Views/login.php
+++ ControlReparacions/app/Views/login.php
@@ -11,11 +11,11 @@
 
 <style>
     body {
-        font-family: system-ui;   
+        font-family: system-ui;
     }
 </style>
 
-<body class="h-screen flex flex-col justify-center items-center" style="background-image: url('<?=base_url()?>/assets/img/login.jpg') ; background-repeat: no-repeat; background-attachment: fixed; background-size: cover;">
+<body class="h-screen flex flex-col justify-center items-center" style="background-image: url('<?= base_url() ?>/assets/img/login.jpg') ; background-repeat: no-repeat; background-attachment: fixed; background-size: cover;">
 
     <section class="bg-[#ffffff] text-terciario-4 flex flex-col gap-5 items-center border-2 border-secundario rounded-md px-20 py-10">
         <div class="flex items-center" style="user-select: none">
@@ -28,7 +28,7 @@
             <!-- USUARIO -->
             <div>
                 <div class="relative flex items-center">
-                    <input name="user" id="user" type="text" required class="w-full  text-sm border-b border-gray-300 focus:border-[#333] px-2 py-3 outline-none" placeholder="<?=lang('forms.user')?> *" value="<?=(!empty($_COOKIE['user']))?$_COOKIE['user']:''?>" />
+                    <input name="user" id="user" type="text" required class="w-full  text-sm border-b border-gray-300 focus:border-[#333] px-2 py-3 outline-none" placeholder="<?= lang('forms.user') ?> *" value="<?= (!empty($_COOKIE['user'])) ? $_COOKIE['user'] : '' ?>" />
                     <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
                         <defs>
                             <clipPath id="a" clipPathUnits="userSpaceOnUse">
@@ -46,7 +46,7 @@
             <!-- CONTRASEÑA -->
             <div class="mt-8">
                 <div class="relative flex items-center">
-                    <input name="password" id="password" type="password" required class="w-full  text-sm border-b border-gray-300 focus:border-[#333] px-2 py-3 outline-none" placeholder="<?= lang('forms.passwd') ?> *"/>
+                    <input name="password" id="password" type="password" required class="w-full  text-sm border-b border-gray-300 focus:border-[#333] px-2 py-3 outline-none" placeholder="<?= lang('forms.passwd') ?> *" />
                     <svg xmlns="http://www.w3.org/2000/svg" id="buttonShow" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2 cursor-pointer" viewBox="0 0 128 128">
                         <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
                     </svg>
@@ -57,9 +57,9 @@
             <div class="flex gap-10 w-80 text-sm">
 
                 <div class="flex items-center">
-                    <input id="remember" name="remember" type="checkbox" class="h-4 w-4 shrink-0  border-gray-300 rounded" <?=(!empty($_COOKIE['user']))?'checked':''?> />
+                    <input id="remember" name="remember" type="checkbox" class="h-4 w-4 shrink-0  border-gray-300 rounded" <?= (!empty($_COOKIE['user'])) ? 'checked' : '' ?> />
                     <label for="remember" class="ml-3 block hover:text-blue-900 hover:underline cursor-pointer">
-                        <?=lang('login.remember')?>
+                        <?= lang('login.remember') ?>
                     </label>
                 </div>
 
@@ -83,11 +83,11 @@
             <?php endif ?>
         </form>
 
-        <p class="text-terciario-2"><?=lang('login.next')?></p>
+        <p class="text-terciario-2"><?= lang('login.next') ?></p>
 
-        <a href="<?= $client ?>" class="px-4 py-2 border flex gap-2 border-slate-700 rounded-lg text-slate-700 hover:bg-blue-400 hover:border-slate-400 hover:text-white hover:shadow transition duration-150">
-            <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo">
-            <span><?=lang('login.google')?></span>
+        <!-- Login Google  -->
+        <a href="<?= $client ?>" class="px-4 py-2 border flex gap-2 border-slate-700 rounded-lg text-slate-700 hover:bg-red-300 hover:border-slate-400 hover:text-white hover:shadow transition duration-150">
+            <img src="/assets/img/logoxtec.png" alt="Logo" width="80px" class="mb-3.5 ml-2">
         </a>
 
     </section>
@@ -101,7 +101,7 @@
     buttonShow.addEventListener('click', () => {
         const isPasswordVisible = password.type === 'text';
         password.type = isPasswordVisible ? 'password' : 'text';
-       
+
     });
 </script>
 
